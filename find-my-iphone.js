750c5e47656e5bcb2a6535c546ec42d5ff9fb607
Raphael Voellmy <r.voellmy@gmail.com>
2017-05-10 21:52:58 +0200

    format code

--- find-my-iphone.js
+++ find-my-iphone.js
@@ -1,50 +1,50 @@
 var icloud = require('find-my-iphone').findmyphone;
 
-module.exports = function(RED) {
+module.exports = function (RED) {
 
-	function FindMyIphone(config) {
-		RED.nodes.createNode(this, config);
-		var node = this;
+  function FindMyIphone(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
 
-		icloud.apple_id = this.credentials.username
-		icloud.password = this.credentials.password
-    
+    icloud.apple_id = this.credentials.username
+    icloud.password = this.credentials.password
+
     findMyPhone = icloud.findmyphone;
 
-		this.on('input', function(msg) {
-			node.status({fill:"yellow",shape:"ring",text:"get device data..."});
-			icloud.getDevices(function(err, devices) {
+    this.on('input', function (msg) {
+      node.status({fill: "yellow", shape: "ring", text: "get device data..."});
+      icloud.getDevices(function (err, devices) {
 
         // failed to get device information
-				if(err || !devices) {
-					node.error('Could not connect to icloud', err);
-					node.status({fill:"red",shape:"dot",text:"Connection failed"});
-          setTimeout(function() {
+        if (err || !devices) {
+          node.error('Could not connect to icloud', err);
+          node.status({fill: "red", shape: "dot", text: "Connection failed"});
+          setTimeout(function () {
             node.status({});
           }, 10000)
-					return;
-				}
+          return;
+        }
 
-        node.status({fill:"green",shape:"dot",text:"Connected"});
+        node.status({fill: "green", shape: "dot", text: "Connected"});
 
         // send all devices to the same output: [[msg, msg]]
-        node.send([devices.map(function(device) {
-          return { payload: device }
+        node.send([devices.map(function (device) {
+          return {payload: device}
         })]);
 
         // clear the status
-        setTimeout(function() {
+        setTimeout(function () {
           node.status({});
         }, 2000)
 
-			});
-		});
-	}
+      });
+    });
+  }
 
-	RED.nodes.registerType("find-my-iphone", FindMyIphone, {
-		credentials: {
-			username: {type:"text"},
-			password: {type:"password"}
-		}
-	});
+  RED.nodes.registerType("find-my-iphone", FindMyIphone, {
+    credentials: {
+      username: {type: "text"},
+      password: {type: "password"}
+    }
+  });
 }
